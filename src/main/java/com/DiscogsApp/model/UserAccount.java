bf199002b60839849f69093f2216eb90ac9e2458
Trevor Davies <trevordavies095@gmail.com>
2017-11-30 16:44:57 -0500

    Updated UserAccount.java documentation.

--- src/main/java/com/DiscogsApp/model/UserAccount.java
+++ src/main/java/com/DiscogsApp/model/UserAccount.java
@@ -3,18 +3,25 @@ package com.DiscogsApp.model;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
-public class UserAccount {
-
+public class UserAccount
+{
+    // Class constants
     private final String firstname;
-
     private final String lastname;
-
     private final String username;
 
+    // Class variables
     private ArrayList<String> songsRated;
 
     public UserAccount(String firstname, String lastname, String username,
-                       ArrayList<String> songsRated){
+                       ArrayList<String> songsRated)
+    {
+        // Local constants
+
+        // Local variables
+
+        /****** start UserAccount() ******/
+
         this.firstname = firstname;
         this.lastname = lastname;
         this.username = username;
@@ -24,15 +31,12 @@ public class UserAccount {
     public String getFirstname() {
         return firstname;
     }
-
     public String getLastname() {
         return lastname;
     }
-
     public String getUsername() {
         return username;
     }
-
     public ArrayList<String> getSongsRated() {
         return songsRated;
     }
