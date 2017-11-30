28363b66b980804f766bf7341aaa1c9d19ddfef2
Trevor Davies <trevordavies095@gmail.com>
2017-11-30 17:34:39 -0500

    Updated PostUpdateAccountRoute.java documentation.

--- src/main/java/com/DiscogsApp/ui/PostUpdateAccountRoute.java
+++ src/main/java/com/DiscogsApp/ui/PostUpdateAccountRoute.java
@@ -8,16 +8,24 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
-public class PostUpdateAccountRoute implements Route {
-
+public class PostUpdateAccountRoute implements Route
+{
+    // Class constants
     private final TemplateEngine templateEngine;
-
     private final SQLManager sqlManager;
-
     private final SearchCache searchCache;
 
+    // Class variables
+
     public PostUpdateAccountRoute(TemplateEngine templateEngine,
-                                 SQLManager sqlManager, SearchCache searchCache){
+                                 SQLManager sqlManager, SearchCache searchCache)
+    {
+        // Local constants
+
+        // Local variables
+
+        /****** start PostUpdateAccountRoute() ******/
+
         Objects.requireNonNull(templateEngine, "templateEngine must not be null");
         Objects.requireNonNull(sqlManager, "SQLManager must not be null");
         Objects.requireNonNull(searchCache, "searchCache must not be null");
@@ -26,29 +34,33 @@ public class PostUpdateAccountRoute implements Route {
         this.templateEngine = templateEngine;
     }
 
-    public String handle(Request request, Response response) {
+    public String handle(Request request, Response response)
+    {
         // Local constants
         final Session httpSession = request.session();
         final Map<String, Object> vm = new HashMap<>();
 
         // Local variables
+        String newUsername = request.queryParams("updateUName");
+        String newFirstname =  request.queryParams("updateFName");
+        String newLastname = request.queryParams("updateLName");
+
+        /****** start handle() ******/
 
         if(httpSession.isNew()){
             response.redirect(Routes.HOME_URL);
             return null;
         }
 
-        /****** start handle() ******/
-
         vm.put(FTLKeys.SIGNED_IN, httpSession.attribute(FTLKeys.SIGNED_IN));
         vm.put(FTLKeys.USER, httpSession.attribute(FTLKeys.USER));
         vm.put(FTLKeys.ADMIN, httpSession.attribute(FTLKeys.ADMIN));
-        String newUsername = request.queryParams("updateUName");
-        String newFirstname =  request.queryParams("updateFName");
-        String newLastname = request.queryParams("updateLName");
-        if(!newFirstname.equals("")) sqlManager.updateFirstname(
+
+        if(!newFirstname.equals(""))
+            sqlManager.updateFirstname(
                 httpSession.attribute(FTLKeys.USER), newFirstname);
-        if(!newLastname.equals("")) sqlManager.updateLastname(
+        if(!newLastname.equals(""))
+            sqlManager.updateLastname(
                 httpSession.attribute(FTLKeys.USER), newLastname);
         if(!newUsername.equals("")){
             sqlManager.updateUsername(httpSession.attribute(FTLKeys.USER), newUsername);
