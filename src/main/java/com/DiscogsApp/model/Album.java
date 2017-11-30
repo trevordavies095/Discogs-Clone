a09811d5d2f8eb7c97def5941fb11052026b3514
Trevor Davies <trevordavies095@gmail.com>
2017-11-30 16:25:48 -0500

    Updated Album.java documentation.

--- src/main/java/com/DiscogsApp/model/Album.java
+++ src/main/java/com/DiscogsApp/model/Album.java
@@ -1,34 +1,40 @@
 package com.DiscogsApp.model;
 
-public class Album implements SearchObject {
-
+public class Album implements SearchObject
+{
+    // Class constants
     private static final SearchEnum type = SearchEnum.ALBUM;
-
     private final String barcode;
-
     private final String style;
-
     private final String genre;
-
     private final String title;
-
     private final double rating;
-
     private final Artist artist;
 
-    public Album(String barcode, String style, String genre, String title, int rating, Artist artist){
+    // Class variables
+
+    public Album(String barcode, String style, String genre, String title, int rating, Artist artist)
+    {
+        // Local constants
+
+        // Local variables
+
+        /****** start Album() ******/
+
         this.barcode = barcode;
-        if(genre != null) {
+
+        if(genre != null)
             this.genre = genre;
-        } else {
+        else
             this.genre = "unknown";
-        }
+
         this.title = title;
-        if(style != null) {
+
+        if(style != null)
             this.style = style;
-        } else {
+        else
             this.style = "unknown";
-        }
+
         this.rating = rating;
         this.artist = artist;
     }
@@ -64,17 +70,33 @@ public class Album implements SearchObject {
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+    public boolean equals(Object o)
+    {
+        // Local constants
 
-        Album album = (Album) o;
+        // Local variables
+        Album album;
 
-        if (rating != album.rating) return false;
-        if (barcode != null ? !barcode.equals(album.barcode) : album.barcode != null) return false;
-        if (style != null ? !style.equals(album.style) : album.style != null) return false;
-        if (genre != null ? !genre.equals(album.genre) : album.genre != null) return false;
-        if (artist != null ? !artist.equals(album.artist) : album.artist != null) return false;
+        /****** start equals() ******/
+
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+
+        album = (Album) o;
+
+        if (rating != album.rating)
+            return false;
+        if (barcode != null ? !barcode.equals(album.barcode) : album.barcode != null)
+            return false;
+        if (style != null ? !style.equals(album.style) : album.style != null)
+            return false;
+        if (genre != null ? !genre.equals(album.genre) : album.genre != null)
+            return false;
+        if (artist != null ? !artist.equals(album.artist) : album.artist != null)
+            return false;
+
         return title != null ? title.equals(album.title) : album.title == null;
     }
 }
