efc3610f9a524f0bba2c039343dd30f65ce45bb5
Trevor Davies <trevordavies095@gmail.com>
2017-11-30 16:43:07 -0500

    Updated URSong.java documentation.

--- src/main/java/com/DiscogsApp/model/URSong.java
+++ src/main/java/com/DiscogsApp/model/URSong.java
@@ -1,17 +1,23 @@
 package com.DiscogsApp.model;
 
+public class URSong implements Comparable<URSong>
+{
+    // Class constants
 
-public class URSong implements Comparable<URSong> {
-
+    // Class variables
     private String title;
-
     private String artistName;
-
     private String albumName;
-
     private int rating;
 
-    public URSong(String title, String albumName, String artistName, int rating){
+    public URSong(String title, String albumName, String artistName, int rating)
+    {
+        // Local constants
+
+        // Local variables
+
+        /****** start URSong() ******/
+
         this.title = title;
         this.rating = rating;
         this.artistName = artistName;
@@ -21,19 +27,22 @@ public class URSong implements Comparable<URSong> {
     public int getRating(){
         return this.rating;
     }
-
     public String getTitle(){
         return this.title;
     }
-
     public String getArtistName(){
         return this.artistName;
     }
 
     @Override
-    public int compareTo(URSong u){
+    public int compareTo(URSong u)
+    {
+        // Local constants
+
+        // Local variables
+
+        /****** start compareTo() ******/
+
         return u.getRating() - this.rating;
     }
-
-
 }
