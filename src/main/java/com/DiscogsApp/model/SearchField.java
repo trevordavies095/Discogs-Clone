986067082e383fdf5a3cdcdf683149ce9aebf119
Trevor Davies <trevordavies095@gmail.com>
2017-11-30 16:38:27 -0500

    Updated SearchField.java documentation.

--- src/main/java/com/DiscogsApp/model/SearchField.java
+++ src/main/java/com/DiscogsApp/model/SearchField.java
@@ -1,13 +1,21 @@
 package com.DiscogsApp.model;
 
-public class SearchField {
-
+public class SearchField
+{
+    // Class constants
     private final SearchEnum type;
-
     private final String value;
 
+    // Class variables
 
-    public SearchField(SearchEnum type, String value){
+    public SearchField(SearchEnum type, String value)
+    {
+        // Local constants
+
+        // Local variables
+
+        /****** start SearchField() ******/
+
         this.type = type;
         this.value = value;
     }
@@ -15,23 +23,18 @@ public class SearchField {
     public SearchEnum getType() {
         return this.type;
     }
-
     public String getTable() {
         return type.getTable();
     }
-
     public String getfrName() {
         return type.getfrName();
     }
-
     public String getFkey(){
         return type.getFkey();
     }
-
     public String getValue(){
         return this.value;
     }
-
     public String getPkey(){
         return type.getPkey();
     }
