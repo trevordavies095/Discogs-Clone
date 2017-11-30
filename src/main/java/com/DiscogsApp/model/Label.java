72615977532be5bad4f362b02e9101269b2115ee
Trevor Davies <trevordavies095@gmail.com>
2017-11-30 16:36:47 -0500

    Updated Label.java documentation.

--- src/main/java/com/DiscogsApp/model/Label.java
+++ src/main/java/com/DiscogsApp/model/Label.java
@@ -1,16 +1,23 @@
 package com.DiscogsApp.model;
 
-public class Label implements SearchObject {
-
+public class Label implements SearchObject
+{
+    // Class constants
     private static final SearchEnum type = SearchEnum.LABEL;
-
     private final String name;
-
     private final int formed;
-
     private final double netWorth;
 
-    public Label(String name, int formed, double netWorth){
+    // Class variables
+
+    public Label(String name, int formed, double netWorth)
+    {
+        // Local constants
+
+        // Local variables
+
+        /****** start Label() ******/
+
         this.formed = formed;
         this.name = name;
         this.netWorth = netWorth;
@@ -19,32 +26,41 @@ public class Label implements SearchObject {
     public static SearchEnum getType() {
         return type;
     }
-
     public int getFormed() {
         return formed;
     }
-
     public String getName() {
         return name;
     }
-
     public double getNetWorth() {
         return netWorth;
     }
-
     public String toString(){
         return name;
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+    public boolean equals(Object o)
+    {
+        // Local constants
 
-        Label label = (Label) o;
+        // Local variables
+        Label label;
 
-        if (formed != label.formed) return false;
-        if (Double.compare(label.netWorth, netWorth) != 0) return false;
+        /****** start equals() ******/
+
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+
+        label = (Label) o;
+
+        if (formed != label.formed)
+            return false;
+        if (Double.compare(label.netWorth, netWorth) != 0)
+            return false;
+
         return name != null ? name.equals(label.name) : label.name == null;
     }
 
