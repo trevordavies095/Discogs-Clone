1b2051081cacff32c555012e533070663e9a7492
Trevor Davies <trevordavies095@gmail.com>
2017-11-30 16:34:53 -0500

    Updated DiscEvent.java documentation.

--- src/main/java/com/DiscogsApp/model/DiscEvent.java
+++ src/main/java/com/DiscogsApp/model/DiscEvent.java
@@ -1,21 +1,26 @@
 package com.DiscogsApp.model;
 
-public class DiscEvent {
+public class DiscEvent
+{
+    // Class constants
 
+    // Class variables
     private String eventName;
-
     private String eventTime;
-
     private String eventLocation;
-
     private String eventArtist;
-
     private int eventID;
-
     private int attendees;
 
     public DiscEvent(String eventName, String eventTime, String eventArtist,
-                     String eventLocation, int ID){
+                     String eventLocation, int ID)
+    {
+        // Local constants
+
+        // Local variables
+
+        /****** start DiscEvent() ******/
+
         this.eventArtist = eventArtist;
         this.eventLocation = eventLocation;
         this.eventName = eventName;
@@ -23,42 +28,48 @@ public class DiscEvent {
         this.eventID = ID;
     }
 
-    public String getEventName() {
-        return eventName;
-    }
+    public static String parseTimestamp(String eTime)
+    {
+        // Local constants
 
-    public static String parseTimestamp(String eTime) {
+        // Local variables
         String [] inter = eTime.split(" ");
         String [] date = inter[0].split("-");
         String [] time = inter[1].split(":");
         int ogHours = Integer.parseInt(time[0]) - 12;
         String ampm = "AM";
-        if(ogHours >= 0) ampm = "PM";
-        if(ogHours == 0) ogHours = 12;
-        String hours = Integer.toString(Math.abs(ogHours));
+        String hours;
+
+        /****** start parseTimestamp() ******/
+
+        if(ogHours >= 0)
+            ampm = "PM";
+        if(ogHours == 0)
+            ogHours = 12;
+
+        hours = Integer.toString(Math.abs(ogHours));
+
         return date[2] + "/" + date[1] + "/" + date[0] + " at " + hours + ":" + time[1] + ampm;
     }
 
     public String getEventTime(){
         return parseTimestamp(this.eventTime);
     }
-
     public String getRawTime(){
         return this.eventTime;
     }
-
     public String getEventLocation() {
         return eventLocation;
     }
-
     public String getEventArtist() {
         return eventArtist;
     }
-
+    public String getEventName() {
+        return eventName;
+    }
     public int getAttendees() {
         return attendees;
     }
-
     public int getEventID() {
         return eventID;
     }
