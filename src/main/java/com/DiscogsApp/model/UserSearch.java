101caad601b0cb8f7e9d5367faa1877164ffaaca
Trevor Davies <trevordavies095@gmail.com>
2017-11-30 16:45:56 -0500

    Updated UserSearch.java documentation.

--- src/main/java/com/DiscogsApp/model/UserSearch.java
+++ src/main/java/com/DiscogsApp/model/UserSearch.java
@@ -2,15 +2,25 @@ package com.DiscogsApp.model;
 
 import java.util.ArrayList;
 
-public class UserSearch {
+public class UserSearch
+{
+    // Class constants
 
+    // Class variables
     private ArrayList<SearchObject> songs;
     private ArrayList<SearchObject> albums;
     private ArrayList<SearchObject> artists;
     private ArrayList<SearchObject> labels;
 
     public UserSearch(ArrayList<SearchObject> songs, ArrayList<SearchObject> albums,
-                      ArrayList<SearchObject> artists, ArrayList<SearchObject> labels){
+                      ArrayList<SearchObject> artists, ArrayList<SearchObject> labels)
+    {
+        // Local constants
+
+        // Local variables
+
+        /****** start UserSearch() ******/
+
         this.songs = songs;
         this.albums = albums;
         this.artists = artists;
@@ -20,15 +30,12 @@ public class UserSearch {
     public ArrayList<SearchObject> getSongs() {
         return songs;
     }
-
     public ArrayList<SearchObject> getAlbums() {
         return albums;
     }
-
     public ArrayList<SearchObject> getArtists() {
         return artists;
     }
-
     public ArrayList<SearchObject> getLabels() {
         return labels;
     }
