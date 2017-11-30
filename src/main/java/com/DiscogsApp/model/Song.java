6bda0e61b7edb3ba49e7893eb89f1ca22234337d
Trevor Davies <trevordavies095@gmail.com>
2017-11-30 16:41:38 -0500

    Updated Song.java documentation.

--- src/main/java/com/DiscogsApp/model/Song.java
+++ src/main/java/com/DiscogsApp/model/Song.java
@@ -1,102 +1,116 @@
 package com.DiscogsApp.model;
 
-public class Song implements SearchObject {
-
+public class Song implements SearchObject
+{
+    // Class constants
     public static final SearchEnum type = SearchEnum.SONG;
-
     private final String title;
-
     private final String length;
-
     private final double rating;
-
     private final String genre;
-
     private final boolean explicit;
-
     private final int releaseYear;
-
     private final Album album;
-
     private final int ID;
 
-    private String getExplicit(){
-        if(explicit){
-            return "Yes";
-        }else{
-            return "No";
-        }
-    }
+    // Class variables
 
     public Song(int sumrating, int numrating, int releaseYear, boolean explicit,
-                String title, String length, String genre, Album album, int ID){
-        if(genre != null) {
+                String title, String length, String genre, Album album, int ID)
+    {
+        // Local constants
+
+        // Local variables
+
+        /****** start Song() ******/
+
+        if(genre != null)
             this.genre = genre;
-        } else {
+        else
             this.genre = "unknown";
-        }
+
         this.title = title;
         this.length = length;
         this.explicit = explicit;
         this.releaseYear = releaseYear;
         this.album = album;
         this.ID = ID;
-        if(sumrating != 0 && numrating != 0) {
+
+        if(sumrating != 0 && numrating != 0)
             this.rating = (double)sumrating / (double)numrating;
-        } else {
+        else
             this.rating = 0;
-        }
+    }
+
+    private String getExplicit()
+    {
+        // Local constants
+
+        // Local variables
+
+        /****** start getExplicit() ******/
+
+        if(explicit)
+            return "Yes";
+        else
+            return "No";
     }
 
     public static SearchEnum getType() {
         return type;
     }
-
     public String getTitle() {
         return title;
     }
-
     public String getLength() {
         return length;
     }
-
     public double getRating() {
         return rating;
     }
-
     public String getGenre() {
         return genre;
     }
-
     public String isExplicit() {
         return getExplicit();
     }
-
     public int getReleaseYear() {
         return releaseYear;
     }
-
     public Album getAlbum(){ return album; }
-
     public int getID(){ return ID; }
-
     public String toString(){
         return title + " by " + album.getArtist().getName();
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+    public boolean equals(Object o)
+    {
+        // Local constants
 
-        Song song = (Song) o;
+        // Local variables
+        Song song;
 
-        if (rating != song.rating) return false;
-        if (explicit != song.explicit) return false;
-        if (releaseYear != song.releaseYear) return false;
-        if (!title.equals(song.title)) return false;
-        if (!length.equals(song.length)) return false;
+        /****** start equals() ******/
+
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+
+        song = (Song) o;
+
+        if (rating != song.rating)
+            return false;
+        if (explicit != song.explicit)
+            return false;
+        if (releaseYear != song.releaseYear)
+            return false;
+        if (!title.equals(song.title))
+            return false;
+        if (!length.equals(song.length))
+            return false;
+
         return genre.equals(song.genre);
     }
-
 }
