cb0a21747638e3beeed5e41665e94534763c1e4b
Trevor Davies <trevordavies095@gmail.com>
2017-11-30 15:54:04 -0500

    Updated SearchCache.java documentation.

--- src/main/java/com/DiscogsApp/appl/SearchCache.java
+++ src/main/java/com/DiscogsApp/appl/SearchCache.java
@@ -5,69 +5,125 @@ import com.DiscogsApp.model.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class SearchCache {
+public class SearchCache
+{
+    // Class constants
 
+    // Class variables
     private HashMap<String, UserSearch> cache;
 
-    public SearchCache(){
+    public SearchCache()
+    {
+        // Local constants
+
+        // Local variables
+
+        /****** start SearchCache() ******/
+
         this.cache = new HashMap<>();
     }
 
-    public void addUser(String username){
+    public void addUser(String username)
+    {
+        // Local constants
+
+        // Local variables
+
+        /****** start addUser() ******/
+
         cache.put(username, null);
     }
 
-    public void updateCache(String username, UserSearch userSearch){
+    public void updateCache(String username, UserSearch userSearch)
+    {
+        // Local constants
+
+        // Local variables
+
+        /****** start updateCache() ******/
+
         cache.put(username, userSearch);
     }
 
-    public Song getSong(String username, String song){
+    public Song getSong(String username, String song)
+    {
+        // Local constants
+
+        // Local variables
         UserSearch cCache = cache.get(username);
-        if(cCache != null) {
-            ArrayList<SearchObject> data = cCache.getSongs();
-            for (SearchObject curr : data) {
-                if (curr.toString().equals(song) || ((Song) curr).getTitle().equals(song)) {
+        ArrayList<SearchObject> data = cCache.getSongs();
+
+        /****** start getSong() ******/
+
+        if(cCache != null)
+        {
+            for (SearchObject curr : data)
+            {
+                if (curr.toString().equals(song) || ((Song) curr).getTitle().equals(song))
                     return (Song) curr;
-                }
             }
         }
         return null;
     }
 
-    public Album getAlbum(String username, String album){
+    public Album getAlbum(String username, String album)
+    {
+        // Local constants
+
+        // Local variables
         UserSearch cCache = cache.get(username);
-        if(cCache != null) {
-            ArrayList<SearchObject> data = cCache.getAlbums();
-            for (SearchObject curr : data) {
-                if (curr.toString().equals(album) || ((Album) curr).getTitle().equals(album)) {
+        ArrayList<SearchObject> data = cCache.getAlbums();
+
+        /****** start getAlbum() ******/
+
+        if(cCache != null)
+        {
+            for (SearchObject curr : data)
+            {
+                if (curr.toString().equals(album) || ((Album) curr).getTitle().equals(album))
                     return (Album) curr;
-                }
             }
         }
         return null;
     }
 
-    public Artist getArtist(String username, String artist){
+    public Artist getArtist(String username, String artist)
+    {
+        // Local constants
+
+        // Local variables
         UserSearch cCache = cache.get(username);
-        if(cCache != null) {
-            ArrayList<SearchObject> data = cCache.getArtists();
-            for (SearchObject curr : data) {
-                if (curr.toString().equals(artist) || ((Artist) curr).getName().equals(artist)) {
+        ArrayList<SearchObject> data = cCache.getArtists();
+
+        /****** start getArtist() ******/
+
+        if(cCache != null)
+        {
+            for (SearchObject curr : data)
+            {
+                if (curr.toString().equals(artist) || ((Artist) curr).getName().equals(artist))
                     return (Artist) curr;
-                }
             }
         }
         return null;
     }
 
-    public Label getLabel(String username, String label){
+    public Label getLabel(String username, String label)
+    {
+        // Local constants
+
+        // Local variables
         UserSearch cCache = cache.get(username);
-        if(cCache != null) {
-            ArrayList<SearchObject> data = cCache.getLabels();
-            for (SearchObject curr : data) {
-                if (curr.toString().equals(label) || ((Label) curr).getName().equals(label)) {
+        ArrayList<SearchObject> data = cCache.getLabels();
+
+        /****** start getLabel() ******/
+
+        if(cCache != null)
+        {
+            for (SearchObject curr : data)
+            {
+                if (curr.toString().equals(label) || ((Label) curr).getName().equals(label))
                     return (Label) curr;
-                }
             }
         }
         return null;
